313b98b35193a9961c7ecd23e78e24341ba40e5e
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2024-07-27 12:04:51 +0300

    Activity Lifecycle

--- app/src/main/java/com/aliozdemir/helloandroid/MainActivity.kt
+++ app/src/main/java/com/aliozdemir/helloandroid/MainActivity.kt
@@ -1,20 +1,113 @@
 package com.aliozdemir.helloandroid
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        Log.d("ActivityLifecycle", "onCreate1")
+
+        setContentView(R.layout.activity_main)  // onContentChanged() tetiklenir.
+
+        Log.d("ActivityLifecycle", "onCreate2")
+
+        val btnOpenSecondActivity = findViewById<Button>(R.id.btnOpenSecondActivity)
+        btnOpenSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+
+            startActivity(intent)
+            finish()
         }
+
     }
+
+    override fun onContentChanged() {
+        super.onContentChanged()
+
+        Log.d("ActivityLifecycle", "onContentChanged")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("ActivityLifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("ActivityLifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("ActivityLifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("ActivityLifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("ActivityLifecycle", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("ActivityLifecycle", "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val etUserField = findViewById<EditText>(R.id.etUserField)
+        val userField = etUserField.text.toString()
+
+        outState.putString("userField", userField)
+
+        Log.d("ActivityLifecycle", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val userField = savedInstanceState.getString("userField")
+        val etUserField = findViewById<EditText>(R.id.etUserField)
+
+        etUserField.setText(userField)
+
+        Log.d("ActivityLifecycle", "onRestoreInstanceState")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        Log.d("ActivityLifecycle", "onBackPressed")
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+
+        Log.d("ActivityLifecycle", "onUserLeaveHint")       // Home button ya da Recent Apps list tıklanınca
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+
+        Log.d("ActivityLifecycle", "onUserInteraction")     // Kullanıcı UI ile etkileşime geçiyor
+    }
+
 }
---
+++ app/src/main/java/com/aliozdemir/helloandroid/SecondActivity.kt
@@ -0,0 +1,91 @@
+package com.aliozdemir.helloandroid
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("SecondActivityLifecycle", "onCreate1")
+
+        setContentView(R.layout.activity_second)
+
+        Log.d("SecondActivityLifecycle", "onCreate2")
+    }
+
+    override fun onContentChanged() {
+        super.onContentChanged()
+
+        Log.d("SecondActivityLifecycle", "onContentChanged")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("SecondActivityLifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("SecondActivityLifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("SecondActivityLifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("SecondActivityLifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("SecondActivityLifecycle", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("SecondActivityLifecycle", "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.d("SecondActivityLifecycle", "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        Log.d("SecondActivityLifecycle", "onRestoreInstanceState")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        Log.d("SecondActivityLifecycle", "onBackPressed")
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+
+        Log.d("SecondActivityLifecycle", "onUserLeaveHint")       // Home button ya da Recent Apps list tıklanınca
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+
+        Log.d("SecondActivityLifecycle", "onUserInteraction")     // Kullanıcı UI ile etkileşime geçiyor
+    }
+
+}
